514aae09bb5454f8d0f998a683a7056020cfb810
Wilona Shen <wilonashen@dhcp-rhodes-61.eduroam.cornell.edu>
2017-04-24 20:06:01 -0400

    Created view controller

---
+++ lec9/lec9/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lec9
+//
+//  Created by Wilona Shen on 4/24/17.
+//  Copyright © 2017 Wilona Shen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
